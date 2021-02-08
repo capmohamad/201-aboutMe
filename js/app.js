f8d26c1a69d3551f5670dd3cde2e881525cccf0d
mohammad mahmoud mohammad hassouneh <mohammad.hs91@hotmail.com>
2021-02-09 00:46:44 +0200

    newlab

---
+++ js/app.js
@@ -0,0 +1,35 @@
+
+'use strict';
+
+
+let
+
+
+
+
+
+
+
+
+/*let asia=prompt('what asia cuntry you like');
+
+switch(asia.toUpperCase()){
+    case 'ASIA':
+
+
+        console.log(asia.toUpperCase)
+        console.log('asia are big area ');
+        break;
+    case 'india':
+    case 'chaina':
+        console.log('india and chaina are part of asia');
+        break;
+        case'spain':
+        case'france':
+        console.log('spain and france part of EU');
+
+    default:
+        console.log('nice to know');
+}
+
+console.log(asia.toUpperCase());
