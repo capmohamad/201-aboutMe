2c130419d99adb7092c4dc705f6e4aa22a87ffa0
talahajeer <tala.hajeer@gmail.com>
2021-02-10 16:33:21 +0200

    Tala

--- js/app.js
+++ js/app.js
@@ -26,89 +26,96 @@
 
 let score = 0;
 
-let userName = prompt('May I know your name?');
+function A1() {
+    let userName = prompt('May I know your name?');
 
-while (!userName) {
-    userName = prompt('please tell me your name');
-}
+    while (!userName) {
+        userName = prompt('please tell me your name');
+    }
 
-if (userName !== 'mohammed') {
-    alert('too bad youre not mohammed');
-}
+    if (userName !== 'mohammed') {
+        alert('too bad youre not mohammed');
+    }
 
-alert('Hola ' + userName + ' glad to have you here');
+    alert('Hola ' + userName + ' glad to have you here');
+} A1();
 
-let answer = prompt('is my favourtie city Prag', 'answer with yes or no');
+function A2() {
 
-console.log(answer);
+    let answer = prompt('is my favourtie city Prag', 'answer with yes or no');
 
-if (answer.toLowerCase() === 'yes') {
-    alert('good job');
-    score++;
-} else {
-    alert('no thats wrong');
+    console.log(answer);
 
-}
+    if (answer.toLowerCase() === 'yes') {
+        alert('good job');
+        score++;
+    } else {
+        alert('no thats wrong');
 
-let cityArry = ['Prag', 'Medina', 'Amman', 'Jerusalem', 'Moscow', 'Dubai', 'Doha'];
+    }
+} A2();
 
-let check = true;
+function A3() {
+    let cityArry = ['Prag', 'Medina', 'Amman', 'Jerusalem', 'Moscow', 'Dubai', 'Doha'];
+    let check = true;
+    for (let i = 1; i < 7; i++) {
+        let questionOne = prompt('what is most city i visit?');
+
+        for (let ix = 0; ix < cityArry.length; ix++) {
+
+            if (questionOne == cityArry[ix]) {
+                console.log(questionOne, cityArry[ix])
+                alert('that\s right');
+                check = false
+                score++;
+                break;
+            }
 
 
-for (let i = 1; i < 7; i++) {
-    let questionOne = prompt('what is most city i visit?');
+            // let anothercity = 'Prag';
 
-    for (let ix = 0; ix < cityArry.length; ix++) {
-
-        if (questionOne == cityArry[ix]) {
-            console.log(questionOne, cityArry[ix])
-            alert('that\s right');
-            check = false
-            score++;
+            console.log(cityArry[ix]);
+        }
+        if (check == false) {
             break;
         }
-
-
-        // let anothercity = 'Prag';
-
-        console.log(cityArry[ix]);
+        alert('most city i visited' + cityArry);
     }
-    if (check == false) {
-        break;
+} A3();
+function A4() {
+    let question2 = prompt('how many city i like');
+
+    if (question2.toLowerCase() === 'no' || question2.toLowerCase() === 'n') {
+        score++;
+        alert('good job');
+
+    } else {
+        alert('wrong');
     }
-    alert('most city i visited'+ cityArry);
-}
-let question2 = prompt('how many city i like');
+    console.log(score);
+} A4();
 
-if (question2.toLowerCase() === 'no' || question2.toLowerCase() === 'n') {
-    score++
-    score = score + 1
-    alert('good job')
+function A5() {
+    let correctNumber = 20;
+    for (let i = 10; i < 14; i++) {
 
-} else {
-    alert('wrong')
-}
-console.log(score);
+        let gussingGame = prompt('guess a numbber between 10-30');
+        if (gussingGame == correctNumber) {
+            alert('nice answer');
+            score++;
+            break;
+        } else if (gussingGame > correctNumber) { alert('too high'); }
+        else if (gussingGame < correctNumber) {
+            alert('too low ');
+        } else {
+            alert('pleas input number');
+        }
+    }
+
+    alert('the corect number is ' + correctNumber);
+} A5();
 
 alert('score is ' + score);
-
-
-let correctNumber = 20
-for (let i = 10; i < 20; i++) {
-
-    let gussingGame = prompt('guess a numbber between 10-30')
-    if (gussingGame == correctNumber) {
-        alert('nice answer');
-        score++;
-        break;
-    } else if (gussingGame > correctNumber) { alert('too high') }
-    else if (gussingGame < correctNumber) {
-        alert('too low ')
-    } else ('pleas input number')
-    if (i == 15)
-    {alert ('the corect number is ' + correctNumber);
-}
-}
 
 /*function welcomeMessage(uuserName){
     let output= 'nice to see you'+ userName
@@ -129,3 +136,4 @@ do {
     }
     console.log('nice try');
 }
+*/
